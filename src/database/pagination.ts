81d3a2eb2c042dc31cf574cde1774114331ae48d
Lucas Mafra <lucas.mafra95@gmail.com>
2018-07-07 14:46:32 -0300

    removed missing reference file

--- src/database/pagination.ts
+++ src/database/pagination.ts
@@ -1,7 +1,5 @@
-import { ID } from './id'
-
 export interface IPaginationOptions { limit?: number, last?: string }
 export interface IPagination {
     hasMore: boolean,
-    last?: ID
+    last?
 }
