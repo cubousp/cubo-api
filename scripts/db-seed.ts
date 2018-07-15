5a0b2ca7b53895d017211e54d144846ff5dd85b7
Lucas Mafra <lucas.mafra95@gmail.com>
2018-07-15 18:15:36 -0300

    fixed seed script

--- scripts/db-seed.ts
+++ scripts/db-seed.ts
@@ -9,35 +9,30 @@ createActivities([
         endsAt: '2018-09-17T17:00:00.000Z',
         kind: 'LECTURE',
         totalVacancies: 20,
-        visibleForParticipants: true
     },
     {
         title: 'Odontologia veterinária: uma realidade mais do que uma curiosidade',
         startsAt: '2018-09-17T17:00:00.000Z',
         endsAt: '2018-09-17T18:00:00.000Z',
         kind: 'LECTURE',
-        visibleForParticipants: true
     },
     {
         title: 'Atuação do cirurgião-dentista militar nas Forças Armadas – Força Aérea: expectativas, realidades e dificuldades',
         startsAt: '2018-09-17T18:00:00.000Z',
         endsAt: '2018-09-17T19:00:00.000Z',
         kind: 'SURGERY',
-        visibleForParticipants: true
     },
     {
         title: 'Toxina Botulínica e suas possibilidade terapêuticas em DTM',
         startsAt: '2018-09-18T13:00:00.000Z',
         endsAt: '2018-09-18T14:00:00.000Z',
         kind: 'LECTURE',
-        visibleForParticipants: true
     },
     {
         title: 'Mercado de trabalho em odontologia',
         startsAt: '2018-09-18T14:00:00.000Z',
         endsAt: '2018-09-18T15:00:00.000Z',
         kind: 'LECTURE',
-        visibleForParticipants: true
     },
 ])
 
